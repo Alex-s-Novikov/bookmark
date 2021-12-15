1ddd6643fc5b36fe5c6f648d21ec3a21268d6fac
Alex <aleksey.novikov@tatar.ru>
2021-12-15 12:32:20 +0300

    update error

--- src/component/qualitie.jsx
+++ src/component/qualitie.jsx
@@ -10,9 +10,9 @@ const Qualitie = ({ color, name, _id }) => {
 };
 
 Qualitie.propTypes = {
-    color: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    _id: PropTypes.number.isRequired
+    color: PropTypes.string,
+    name: PropTypes.string,
+    _id: PropTypes.string
 };
 
 export default Qualitie;
